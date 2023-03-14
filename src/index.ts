c427e4ff387e369c37ba9ee8b4f4a7851e23bf69
unknown <ezegeek@gmail.com>
2023-03-14 10:41:02 -0300

    Role controller and routes added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello world!')
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import router from './routes/Route';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+app.get('/', (req: Request, res: Response) => {
+    return res.status(200).send({
+        response: "Express Typescript"
+    });
+});
+
+
+app.listen(process.env.APP_PORT, () => {
+    console.log((`${process.env.APP_NAME} running on port ${process.env.APP_PORT}`));
+})
