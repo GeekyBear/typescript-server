4e3cdbc31a08be4231ecd980fe49593d04ed89df
unknown <ezegeek@gmail.com>
2023-06-14 11:08:11 -0300

    Creation of Menu Controller and routes

--- src/routes/Route.ts
+++ src/routes/Route.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import RoleController from '../controllers/RoleController';
-import UserController from '../controllers/UserController';
 import Authorization from '../middleware/Authorization';
 import UserValidation from '../middleware/validation/UserValidation';
+import RoleController from '../controllers/RoleController';
+import UserController from '../controllers/UserController';
+import MasterMenuController from '../controllers/MasterMenuController';
 
 const router = express.Router();
 
+// Role routing
 router.get('/role', Authorization.Authenticated, Authorization.BasicUser, RoleController.GetRole);
 router.post('/role', Authorization.Authenticated, Authorization.AdminRole, RoleController.CreateRole);
 router.patch('/role/:id', Authorization.Authenticated, Authorization.AdminRole, RoleController.UpdateRole);
@@ -18,4 +20,13 @@ router.post('/user/login', UserController.UserLogin);
 router.get('/user/refresh-token', UserController.RefreshToken);
 router.get('/user/current-user', Authorization.Authenticated, UserController.UserDetail);
 router.get('/user/logout', Authorization.Authenticated, UserController.UserLogout);
+
+// Master Menu Routing
+router.post('/menu', Authorization.Authenticated, Authorization.AdminRole, MasterMenuController.CreateMenu);
+router.get('/menu', Authorization.Authenticated, Authorization.AdminRole, MasterMenuController.GetListMenu);
+router.get('/menu/all', Authorization.Authenticated, Authorization.SuperUser, MasterMenuController.GetAllMenu);
+router.get('/menu/:id', Authorization.Authenticated, Authorization.AdminRole, MasterMenuController.GetDetailMenu);
+router.patch('/menu/:id', Authorization.Authenticated, Authorization.AdminRole, MasterMenuController.UpdateMenu);
+router.delete('/menu/:id', Authorization.Authenticated, Authorization.AdminRole, MasterMenuController.SoftDeleteMenu);
+router.delete('/menu/permanent/:id', Authorization.Authenticated, Authorization.SuperUser, MasterMenuController.DeletePermanent);
 export default router;
